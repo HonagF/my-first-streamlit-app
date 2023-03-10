0558a8f3d064fc735dc46ab45dc4bfc089fbc843
Nguyễn Minh Hoàng <69524738+HonagF@users.noreply.github.com>
2023-03-10 22:02:34 +0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,14 @@
+import streamlit as st
+
+st.title('GIẢI PHƯƠNG TRÌNH BẬC NHẤT')
+a = st.number_input('Tham số a')
+b = st.number_input('Tham số b')
+
+if st.button('Giải'):
+  if a == 0 and b == 0: 
+    st.success('Phương trình có vô số nghiệm!')
+  elif a == 0 and b!= 0:
+    st.error('Phương trình vô nghiệm!')
+  elif a!=0:
+    c = -b/a
+    st.success(f'Phương trình có 1 nghiệm: {c}')
